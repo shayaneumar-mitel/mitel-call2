7401a5b437c8defec70d9e9bc05a9605b46afd9e
Shayane Umar <shayane.umar@gmail.com>
2020-11-08 20:45:01 +0530

    Integrate team popup authentication

--- src/app/teams-auth-popup/teams-auth-popup.component.ts
+++ src/app/teams-auth-popup/teams-auth-popup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import AuthService from '../services/AuthService';
+import * as microsoftTeams from '@microsoft/teams-js';
 
 @Component({
   selector: 'app-teams-auth-popup',
@@ -10,6 +12,35 @@ export class TeamsAuthPopupComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if (microsoftTeams) {
+      microsoftTeams.initialize(<any>window);
+      microsoftTeams.getContext((context) => {
+        if (context) {
+          // If here we have a Teams context. Ensure we're logged in
+          // and then request the access token.
+          if (!AuthService.isLoggedIn()) {
+            AuthService.login(['User.Read', 'User.ReadBasic.All', 'Directory.Read.All']);
+            // This call won't return - catch it on the redirect
+          } else {
+            AuthService.getAccessTokenEx(['User.Read', 'User.ReadBasic.All', 'Directory.Read.All'])
+              .then(({ username, accessToken, expiresOn }) => {
+                if (accessToken) {
+                  const response = JSON.stringify({ username, accessToken, expiresOn })
+                  microsoftTeams.authentication.notifySuccess(response);
+                } else {
+                  microsoftTeams.authentication.notifyFailure('Unexpected failure - null token received');
+                }
+              })
+              .catch((error) => {
+                console.log(error);
+                microsoftTeams.authentication.notifyFailure(error);
+              });
+          }
+        }
+      });
+
+
+    }
   }
 
 }
