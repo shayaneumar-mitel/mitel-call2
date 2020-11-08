be484de5f5a9aae12276c81e3c85c09571e0fca1
Shayane Umar <shayane.umar@gmail.com>
2020-11-08 09:58:07 +0530

    Integrate Tab component

--- src/app/tab/tab.component.ts
+++ src/app/tab/tab.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as MicrosoftGraphClient from '@microsoft/microsoft-graph-client';
+import TeamsAuthService from '../services/TeamsAuthService';
+import * as microsoftTeams from '@microsoft/teams-js';
 
 @Component({
   selector: 'app-tab',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab.component.less']
 })
 export class TabComponent implements OnInit {
+  msGraphClient: any;
+  accessToken = null;
+  messages = [];
+  name = '';
+  error = '';
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.msGraphClient = MicrosoftGraphClient.Client.init({
+      authProvider: async (done) => {
+        if (!this.accessToken) {
+          const token = await TeamsAuthService
+            .getAccessToken(['User.Read', 'User.ReadBasic.All', 'Directory.Read.All'],
+              microsoftTeams);
+          this.accessToken = token;
+        }
+        done(null, this.accessToken);
+      }
+    });
+  }
+
+  getMessages() {
+    this.msGraphClient
+      .api('users')
+      .filter(`startswith(displayName,'${this.name}')`)
+      .get(async (error, rawMessages, rawResponse) => {
+        if (!error) {
+          this.messages = rawMessages.value;
+        } else {
+          this.error = error;
+        }
+      });
   }
 
 }
