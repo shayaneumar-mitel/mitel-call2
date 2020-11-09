4549273d281924ee65b56adf6b30da25fe062690
Shayane Umar <shayane.umar@gmail.com>
2020-11-09 08:46:15 +0530

    Support running app on port 3000

--- src/app/Config.js
+++ src/app/Config.js
@@ -1,6 +1,6 @@
 export const clientId = "bd058f85-2ba7-47fe-bc12-1f8061beacbc";
 export const authority = "https://login.microsoftonline.com/4bff5a2b-b30d-4939-81ff-8f76138347df";
-export const redirectUri = "https://localhost:4200/";
+export const redirectUri = "https://localhost:3000/";
 // export const redirectUri = " https://b35d11cab154.ngrok.io";
 // export const redirectUri = "https://d3nn161xdywwor.cloudfront.net";
 
